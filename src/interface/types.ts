e836db2990bf9c5bb821328ba627cec6f45a9a0b
skaimarvas <skaimarvas@gmail.com>
2025-06-10 21:25:56 +0300

    feat: add Skip interface to define skip properties

---
+++ src/interface/types.ts
@@ -0,0 +1,12 @@
+export interface Skip {
+  id: string;
+  name: string;
+  size: string;
+  description: string;
+  price: number;
+  image: string;
+  dimensions: string;
+  capacity: string;
+  popular?: boolean;
+  features: string[];
+}
